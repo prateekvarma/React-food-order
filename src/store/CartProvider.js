f5275b890933f146cc4dbdbbd281a55b5a5e222e
Prateek Varma <prtk.varma@gmail.com>
2022-09-02 16:46:48 -0600

    Created Context and extracted it's logic to it's own file, and wrapped it around all components

---
+++ src/store/CartProvider.js
@@ -0,0 +1,23 @@
+import CartContext from "./cart-context";
+
+const CartProvider = (props) => {
+  const addItemToCartHandler = (item) => {};
+
+  const removeItemFromCartHandler = (id) => {};
+
+  const cartContextData = {
+    //This data object will be dynamic
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContextData}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
---
+++ src/store/cart-context.js
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+const CartContext = createContext({
+  //initialize the context - this can be omitted, but helps the IDE with auto completion
+  items: [],
+  totalAmount: 0,
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+export default CartContext;
