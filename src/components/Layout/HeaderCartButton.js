310f7c068a93e31d6b8a65bc879e06ed197e6cdc
Prateek Varma <prtk.varma@gmail.com>
2022-09-02 16:58:13 -0600

    Used context API to calculate the number of items for the cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,21 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
+
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((currentNum, item) => currentNum + item.amount, 0);
+
   return (
     <button className={classes.button} onClick={props.onHeaderCartClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3 {/* Dummy value */}</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
