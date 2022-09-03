aabc9d9a8d8c9d9b70c28b8eb91d15e0502bca6f
Prateek Varma <prtk.varma@gmail.com>
2022-09-03 14:46:34 -0600

    Created CartItem component for each individual item, and passed respective props to it. Functions to add/remove items from here is yet to be written

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
 import CartContext from "../../store/cart-context";
 
@@ -10,10 +11,25 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemAddHandler = (item) => {
+
+  }
+
+  const cartItemRemoveHandler = (id) => {
+
+  }
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => {
-        return <li>{item.name}</li>;
+        return <CartItem 
+                key={item.id} 
+                name={item.name} 
+                amount={item.amount} 
+                price={item.price} 
+                onRemove={cartItemRemoveHandler.bind(null, item.id)} 
+                onAdd={cartItemAddHandler.bind(null, item)} 
+                />
       })}
     </ul>
   );
