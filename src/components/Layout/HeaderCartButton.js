8f1af1249ae1d3c315b4ef3d07450cdb370b35e5
Prateek Varma <prtk.varma@gmail.com>
2022-09-05 15:14:16 -0600

    Used useEffect and useState to handle button animation whenever cart items are changed

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,32 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItems = cartCtx.items.reduce((currentNum, item) => currentNum + item.amount, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if(cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300); //300 because this is when the animation in css ends
+
+    return () => { clearTimeout(timer) }
+  }, [cartCtx.items])
+  
   return (
-    <button className={classes.button} onClick={props.onHeaderCartClick}>
+    <button className={btnClasses} onClick={props.onHeaderCartClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
