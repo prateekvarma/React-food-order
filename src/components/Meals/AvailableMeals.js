ea539cdd43a6e32c5ea51e55100d1a6c56ebdae4
Prateek Varma <prtk.varma@gmail.com>
2022-09-22 11:57:28 -0600

    Created another state to catch http errors, while handling promises

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,11 +7,17 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState(); //undefined as the starting value
   
   useEffect(() => {
     const fetchMeals = async () => {
       setIsLoading(true);
       const response = await fetch("XXX/meals.json");
+      
+      if(!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -26,13 +32,26 @@ const AvailableMeals = () => {
       setMeals(loadedMeals);
       setIsLoading(false);
     }
-    fetchMeals();
+      //Below, we're chaining a  .catch() block instead of a try-catch block, because in a try-catch we would need to create another function and set it as async. This is cleaner.
+      fetchMeals().catch((error) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+      });
+    
   }, []);
 
   if(isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>...Loading</p>
+      </section>
+    )
+  }
+
+  if(httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     )
   }
