ebe4eb379874fcd5702df807eb529429a7172a7b
Prateek Varma <prtk.varma@gmail.com>
2022-09-23 12:51:32 -0600

    Styling changes

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,24 +1,33 @@
+import classes from "./Checkout.module.css"
+
 const Checkout = (props) => {
+    const confirmHandler = (event) => {
+        event.preventDeafault();
+
+    }
+
     return (
-        <form>
-            <div>
+        <form className={classes.form} onSubmit={confirmHandler}>
+            <div className={classes.control}>
                 <label htmlFor="name">Your Name</label>
                 <input id="name" type="text" />
             </div>
-            <div>
+            <div className={classes.control}>
                 <label htmlFor="street">Street</label>
                 <input id="street" type="text" />
             </div>
-            <div>
+            <div className={classes.control}>
                 <label htmlFor="postal">Postal Code</label>
                 <input id="postal" type="text" />
             </div>
-            <div>
+            <div className={classes.control}>
                 <label htmlFor="city">City</label>
                 <input id="city" type="text" />
             </div>
-            <button type="button" onClick={props.onCancel}>Cancel</button>
-            <button>Confirm</button>
+            <div className={classes.actions}>
+                <button type="button" onClick={props.onCancel}>Cancel</button>
+            </div>
+            <button className={classes.submit}>Confirm</button>
         </form>
     )    
 };
