cee65e79012b0391b1d28af5c5d437bbeb07726f
Prateek Varma <prtk.varma@gmail.com>
2022-09-23 12:59:12 -0600

    Used useRef to extract form data (incomplete)

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,8 +1,21 @@
+import { useRef } from "react";
+
 import classes from "./Checkout.module.css"
 
 const Checkout = (props) => {
+    const nameInputRef = useRef();
+    const streetInputRef = useRef();
+    const postalCodeInputRef = useRef();
+    const cityInputRef = useRef();
+
+
     const confirmHandler = (event) => {
         event.preventDeafault();
+
+        const enteredName = nameInputRef.current.value;
+        const enteredStreet = streetInputRef.current.value;
+        const enteredPostalCode = postalCodeInputRef.current.value;
+        const enteredCity = cityInputRef.current.value;
 
     }
 
@@ -10,19 +23,19 @@ const Checkout = (props) => {
         <form className={classes.form} onSubmit={confirmHandler}>
             <div className={classes.control}>
                 <label htmlFor="name">Your Name</label>
-                <input id="name" type="text" />
+                <input ref={nameInputRef} id="name" type="text" />
             </div>
             <div className={classes.control}>
                 <label htmlFor="street">Street</label>
-                <input id="street" type="text" />
+                <input ref={streetInputRef} id="street" type="text" />
             </div>
             <div className={classes.control}>
                 <label htmlFor="postal">Postal Code</label>
-                <input id="postal" type="text" />
+                <input ref={postalCodeInputRef} id="postal" type="text" />
             </div>
             <div className={classes.control}>
                 <label htmlFor="city">City</label>
-                <input id="city" type="text" />
+                <input ref={cityInputRef} id="city" type="text" />
             </div>
             <div className={classes.actions}>
                 <button type="button" onClick={props.onCancel}>Cancel</button>
