60d2bd7f82db3143852a70097fe94782af5fa3b8
Prateek Varma <prtk.varma@gmail.com>
2022-09-23 11:54:51 -0600

    Created a checkout form to finalize the order

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,25 @@
+const Checkout = () => {
+    return (
+        <form>
+            <div>
+                <label htmlFor="name">Your Name</label>
+                <input id="name" type="text" />
+            </div>
+            <div>
+                <label htmlFor="street">Street</label>
+                <input id="street" type="text" />
+            </div>
+            <div>
+                <label htmlFor="postal">Postal Code</label>
+                <input id="postal" type="text" />
+            </div>
+            <div>
+                <label htmlFor="city">City</label>
+                <input id="city" type="text" />
+            </div>
+            <button>Confirm</button>
+        </form>
+    )    
+};
+
+export default Checkout;
